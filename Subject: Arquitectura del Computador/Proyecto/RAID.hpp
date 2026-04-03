b4fac8668f88ccfce7ea2fb81e86c6f9776c8566
ZintosG <simontovar06@gmail.com>
2026-04-03 11:11:48 -0400

    Added writeInfo method in RAID5 Class

--- Subject: Arquitectura del Computador/Proyecto/RAID.hpp
+++ Subject: Arquitectura del Computador/Proyecto/RAID.hpp
@@ -83,7 +83,54 @@ RAID5::RAID5(int num, int size): RAID(num, size){
 }
 
 void RAID5::writeInfo(const string &info){
-    
+    //Obtener numero de bytes a insertar en la fila
+    int segmentsNumber=this->disks.size()-1;
+
+    //Recorrer Info
+    int stringIndex=0;
+    while(stringIndex<info.size()){
+        //Obtener fila actual
+        int currentRow=this->disks[0].getData().size();
+
+        //Calcular en que disco va la paridad en la fila actual
+        int parityDisk=currentRow % this->disks.size();
+
+        //Vector para almacenar los bytes a guardar en la fila
+        vector<unsigned char> currentBytes;
+
+
+        /*Guarda el segmento de bytes a guardar de la fila actual en el vector.
+        Si faltan bytes para terminar la fila rellena con caracter nulo*/
+        for(int i=0; i<segmentsNumber; i++){
+            if(stringIndex<info.size()){
+                currentBytes.push_back(info[stringIndex]);
+                stringIndex++;
+            }else{
+                currentBytes.push_back('\0');
+            }
+        }
+
+        //Calculo de paridad de la fila actual
+        unsigned char parity='\0';
+        for(int j=0; j<currentBytes.size(); j++){
+            parity^=currentBytes[j];
+        }
+
+        /*Insercion en discos, guarda los bytes en orden y el bytes de paridad en el
+        disco correspondiente en la fila*/
+        int insertionIndex=0;
+        for(int k=0; k<this->disks.size(); k++){
+            vector<unsigned char> diskData=this->disks[k].getData();
+            if(k==parityDisk){
+                diskData.push_back(parity);
+            }else{
+                diskData.push_back(currentBytes[insertionIndex]);
+                insertionIndex++;
+            }
+
+            this->disks[k].writeData(diskData);
+        }
+    }
 }
 
 #endif
